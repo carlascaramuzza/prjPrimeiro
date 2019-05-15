930b4626e33884ea8add146e22907613c27145a3
Carla Rodrigues <carla.rodrigues@programmers.com.br>
2019-05-15 08:43:23 -0300

    Meu primeiro commit

---
+++ OlaMundo/OlaMundo/Pages/Error.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OlaMundo.Pages
+{
+   public class ErrorModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Qual é a minha mensagem?";
+        }
+    }
+}
